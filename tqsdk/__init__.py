50c6b770c2d0bac16ea3751338a78320e49bd00c
RenBing <576602329@qq.com>
2019-08-27 16:00:20 +0800

    Add files via upload

---
+++ tqsdk/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+name = "tqsdk"
+
+from tqsdk.wawa import haha
+
---
+++ tqsdk/wawa.py
@@ -0,0 +1,5 @@
+class haha(object):
+    def __init__(self):
+        print('hahha')
+    def run(self):
+        print('haha run!')
